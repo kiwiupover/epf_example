5123818c927ceaf2040f57f937b92f479346784a
Dave Laird <dave@urbanspoon.com>
2013-08-09 11:04:05 -0700

    Fixed bug with caching the contact email

--- app/assets/javascripts/helpers/forms.js
+++ app/assets/javascripts/helpers/forms.js
@@ -1,10 +1,3 @@
 Handlebars.registerHelper('submitButton', function(text) {
   return new Handlebars.SafeString('<button type="submit" class="btn btn-primary">' + text + '</button>');
 });
-
-Handlebars.registerHelper('mailto', function(field) {
-  var address = this.get(field);
-  if (address) {
-    return new Handlebars.SafeString('<a href="mailto: ' + address + '" />' + address + '</a>');
-  }
-});
